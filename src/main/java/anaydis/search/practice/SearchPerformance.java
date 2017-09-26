8b47232c70a1aa573fc2eb5ada9e4f24a4a7758a
Agustin Bettati <bettatiagustin@gmail.com>
2017-09-26 17:09:24 -0300

    Finished performance analysis

--- src/main/java/anaydis/search/practice/SearchPerformance.java
+++ src/main/java/anaydis/search/practice/SearchPerformance.java
@@ -2,12 +2,13 @@ package anaydis.search.practice;
 
 import anaydis.search.ArrayMap;
 import anaydis.search.Map;
+import anaydis.search.RandomizedTreeMap;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Comparator;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.LongSummaryStatistics;
+import java.util.stream.Collectors;
 
 /**
  * @author Agustin Bettati
@@ -15,10 +16,43 @@ import java.util.Comparator;
  */
 public class SearchPerformance {
 
+    private static final int RUNS = 100;
+
     public static void main(String[] args) throws IOException {
-        Map<String, Integer> map = new ArrayMap<>((o1, o2) -> o1.compareTo(o2));
         int[] schemas = {5000, 50000, 100000, 150000, 200000};
 
+        Map<String, Integer> arrayMap = new ArrayMap<>(String::compareTo);
+        Map<String, Integer> treeMap = new RandomizedTreeMap<>(String::compareTo);
+        Map<String,Integer>[] maps = new Map[]{arrayMap,treeMap};
+
+        File file = new File("/Users/agustinbettati/Documents/ArrayVsTreeSearch.csv");
+
+        FileWriter fw = new FileWriter(file);
+
+        fw.write("amount of runs: "+ RUNS + "\n\n");
+        fw.write("n,ArrayMap,RandomizedTreeMap \n");
+
+        for (int schema : schemas) {
+
+            fw.write("" +schema);
+            for (Map<String, Integer> map : maps) {
+                map.clear();
+                fillMap(map, schema);
+
+                List<Long> results = new ArrayList<>();
+                for (int i = 0; i < RUNS; i++) {
+                    results.add(searchInMapAndGetTime(map, schema));
+                }
+                final LongSummaryStatistics statistics =
+                        results.stream().collect(Collectors.summarizingLong(value -> value));
+
+                fw.write("," + String.format("%.0f", statistics.getAverage()) );
+            }
+            fw.write("\n");
+        }
+
+        fw.close();
+        System.out.println("All data has been generated and stored in /Documents/ArrayVsTreeSearch.csv");
 
 
     }
