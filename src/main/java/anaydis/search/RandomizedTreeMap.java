8cda29afe8275c96a90fa746ccc1a03c8f145169
Agustin Bettati <bettatiagustin@gmail.com>
2017-09-25 21:35:12 -0300

    Implemented TreeMap with simple insertion

--- src/main/java/anaydis/search/RandomizedTreeMap.java
+++ src/main/java/anaydis/search/RandomizedTreeMap.java
@@ -2,7 +2,10 @@ package anaydis.search;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * @author Agustin Bettati
@@ -10,10 +13,32 @@ import java.util.Iterator;
  */
 public class RandomizedTreeMap<K,V> implements Map<K,V> {
 
-    @Override
-    public boolean isEmpty() {
-        return false;
+    private Node<K,V>  head;
+    private int size;
+    private Comparator<K> comp;
+
+    public RandomizedTreeMap(Comparator<K> comparator) {
+        head = null;
+        size = 0;
+        comp = comparator;
     }
+
+    private class Node<K,V>{
+        K key;
+        V value;
+        Node<K,V> left;
+        Node<K,V> right;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+            left = null;
+            right = null;
+        }
+
+
+    }
+
 
     @Override
     public int size() {
@@ -22,26 +47,67 @@ public class RandomizedTreeMap<K,V> implements Map<K,V> {
 
     @Override
     public boolean containsKey(@NotNull K key) {
-        return false;
+
+        return find(head, key) != null;
     }
 
     @Override
     public V get(@NotNull K key) {
-        return null;
+        Node<K,V> node = find(head, key);
+        if(node == null) return null;
+        return node.value;
+    }
+
+    private Node<K,V> find(Node<K, V> node, K key) {
+        if(node == null) return null;
+        final int comparison = comp.compare(key, node.key);
+        if(comparison == 0) return node;
+        else if(comparison < 0) return find(node.left, key);
+        else return find(node.right, key);
     }
 
     @Override
     public V put(@NotNull K key, V value) {
-        return null;
+        V prev = get(key);
+        head = put(head, key, value);
+        return prev;
+
     }
+
+    private Node<K, V> put(Node<K, V> node, K key, V value) {
+        if(node == null) {
+                size++;
+                return new Node<K,V>(key, value);
+        }
+        else{
+            final int comparison = comp.compare(key, node.key);
+            if(comparison < 0) node.left = put(node.left, key, value);
+            if(comparison > 0) node.right = put(node.right, key, value);
+            else node.value = value;
+
+            return node;
+        }
+    }
+
 
     @Override
     public void clear() {
-
+        size = 0;
+        head = null;
     }
 
     @Override
     public Iterator<K> keys() {
-        return null;
+        List<K> list = new ArrayList<>();
+        getTreePreOrder(head, list);
+        return list.iterator();
+    }
+
+    private void getTreePreOrder(Node<K,V> node, List<K> list) {
+        if (node != null) {
+            list.add(node.key);
+            getTreePreOrder(node.left, list);
+            getTreePreOrder(node.right, list);
+        }
     }
 }
