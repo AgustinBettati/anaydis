c7b48898c54b25af6dcfe34c9a750f19e4461edc
Agustin Bettati <bettatiagustin@gmail.com>
2017-10-07 11:33:28 -0300

    Added more testing to binary tree

--- src/test/java/anaydis/immutable/BinaryTreeTest.java
+++ src/test/java/anaydis/immutable/BinaryTreeTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.*;
@@ -26,6 +27,19 @@ public class BinaryTreeTest {
         assert(tree3.containsKey(1));
         assert(tree3.containsKey(2));
         assertEquals(tree3.get(0), "Jorge");
+    }
+
+    @Test
+    public void keysIteratorTest(){
+        Map<Integer, String> tree = new BinaryTree<Integer, String>(Integer::compareTo);
+        Map<Integer, String> tree1 = tree.put(0, "Jorge");
+        Map<Integer, String> tree2 = tree1.put(1, "Carlos");
+        Map<Integer, String> tree3 = tree2.put(2, "Perez");
+
+        Iterator<Integer> it = tree3.keys();
+        for (int i = 0; i < 3; i++) {
+            assert(it.next() == i);
+        }
     }
 
 }
